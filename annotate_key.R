be6df78496f548e70dc76e3d757d9c260c20d794
Ryan Valverde de Ward <>
2022-02-04 10:44:57 -0600

    cleanup

--- annotate_key.R
+++ annotate_key.R
@@ -132,3 +132,4 @@ annotated_key <- annotated_key[, .(chromosome,
 																	 type)]
 #/fix
 ################################################################################
+fwrite(annotated_key, "annotated_key.tsv", sep = "\t")
