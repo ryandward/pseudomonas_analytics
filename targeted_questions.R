3da4d6b644dc5dd4e9037444c4395aae35095979
Ryan Valverde de Ward <>
2022-02-09 00:27:27 -0600

    added boxplot maker

--- targeted_questions.R
+++ targeted_questions.R
@@ -1,4 +1,4 @@
-source("counter_salvage.R")
+source("presentation_analysis.R")
 # source("Nb_calculator.R")
 
 CPM_melted <-
@@ -11,12 +11,26 @@ CPM_melted <-
 
 annotated_key <- fread("annotated_key.tsv")
 
-targeted_CPM <- CPM_melted[condition %in% c("Mouse_P1_003",
+targeted_CPM <- CPM_melted[condition %in% c("Inoculum",
+																						"Mouse_P1_003",
 																						"Mouse_P1_006",
 																						"Mouse_P1_015",
 																						"Mouse_P1_016",
 																						"Mouse_P1_017")]
 
+targeted_CPM[condition %in% c("Inoculum", "Mouse_P1_003"),  
+						 # fancy_condition := 'paste0(bold("pelleted "), "inoculum ", t[0])']
+						 Condition := 'Pelleted inoculum t_0']
+						 
+
+targeted_CPM[condition == "Mouse_P1_006",  
+						 # fancy_condition := 'paste0(bold("plated "), "inoculum ", t[0])']
+						 Condition := 'Plated inoculum t_0']
+						 
+targeted_CPM[condition %in% c("Mouse_P1_015", "Mouse_P1_016", "Mouse_P1_017"),  
+						 # fancy_condition := 'paste0(bold("Plated "), italic("ex-vivo "), "10× dilution")']
+						 Condition := 'Plated ex-vivo 10× dilution']
+						 
 targeted_CPM <- exp_design[targeted_CPM, on = .(condition)]
 targeted_CPM <- annotated_key[targeted_CPM, on = .(name == spacer)]
 
@@ -55,21 +69,42 @@ box_CPM <- function(this_gene) {
 	
 	to_plot <-
 		targeted_CPM[gene_name == this_gene |
-								 	locus_tag == this_gene, .(name, gene_name, locus_tag, condition, CPM, verbose, offset, rep)]
+								 	locus_tag == this_gene, .(name, gene_name, locus_tag, condition, CPM, verbose, offset, rep, Condition)]
 	
 	to_plot[, group := gsub("_[0-9]+$", "", verbose) ]
 	
 	to_plot$offset <- factor(as.character(to_plot$offset),
 													 levels = as.character(sort(unique(to_plot$offset))))
 	this_plot <-
-		ggplot(data = to_plot, aes(x = group, y = CPM, fill = offset)) +
-		geom_boxplot() + ggtitle("Box plot") +
-		ggtitle(paste("CPM for guides:", this_gene)) +
-		theme(axis.text.x = element_text(
-			angle = 55,
-			vjust = 1.0,
-			hjust = 1
-		))
+		ggplot(data = to_plot, aes(x = Condition, y = log2(CPM), fill = offset)) +
+		geom_boxplot() + 
+		scale_fill_brewer(palette = "Accent") +
+		ggtitle(paste("CPM for guides:", this_gene))
+	
+	ggthemr("flat")
+	print(this_plot)
+	# rm(this_gene)
+	ggthemr_reset()
+	
+}
+
+box_CPM_controls <- function() {
+	random_controls <-
+		targeted_CPM[name %like% "Ctrl", .(name = unique(name))][sample(.N, 8)]
+	
+	to_plot <-
+		targeted_CPM[name %in% random_controls$name, .(name, gene_name, locus_tag, condition, CPM, verbose, offset, rep, Condition)]
+	
+	to_plot[, group := gsub("_[0-9]+$", "", verbose) ]
+	
+	to_plot$offset <- factor(as.character(to_plot$offset),
+													 levels = as.character(sort(unique(to_plot$offset))))
+	this_plot <-
+		ggplot(data = to_plot, aes(x = Condition, y = log2(CPM), fill = name)) +
+		geom_boxplot() +
+		scale_fill_brewer(palette = "Accent") +
+		ggtitle(paste("Eight Random Guides"))
+
 	
 	ggthemr("flat")
 	print(this_plot)
@@ -94,7 +129,7 @@ plot_CPM_controls <- function() {
 		geom_bar(stat = "identity", position = position_dodge()) +
 		ggtitle(paste("8 Random Control Guides. CPM")) +
 		theme(axis.text.x = element_text(
-			angle = 55,
+			angle = 0,
 			vjust = 1.0,
 			hjust = 1
 		))
