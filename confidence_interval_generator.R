55dc7de2192ba365efa33d966a3fb96a81df7960
Ryan Valverde de Ward <>
2022-02-12 12:34:49 -0600

    renamed internal variables to something more intuitive

--- confidence_interval_generator.R
+++ confidence_interval_generator.R
@@ -19,83 +19,83 @@ CPM_melted <- melt(
 
 # For future experiments, consider "sample" vs "condition".
 
-interest_CPM <- copy(CPM_melted)
+grouped_CPM <- copy(CPM_melted)
 
-interest_CPM[
+grouped_CPM[
 	condition %in% c("Inoculum", "Mouse_P1_003"),  
 	Condition := 'Pelleted inoculum t_0']
 
-# interest_CPM[
+# grouped_CPM[
 # 	condition == "Mouse_P1_006",
 # 	Condition := 'Plated inoculum t_0']
 
-interest_CPM[
+grouped_CPM[
 	condition %in% c("Mouse_P1_015", "Mouse_P1_016", "Mouse_P1_017"),  
 	Condition := 'Plated ex-vivo 10× dilution']
 
-interest_CPM[
+grouped_CPM[
 	condition %in% c("Mouse_P1_018", "Mouse_P1_019", "Mouse_P1_020", "Mouse_P1_021", "Mouse_P1_022"),  
 	Condition := 'Plated ex-vivo 100× dilution']
 
-interest_CPM[
+grouped_CPM[
 	condition %in% c("Mouse_P1_007", "Mouse_P1_008"),  
 	Condition := 'Pelleted ex-vivo 10× dilution']
 
 ########################################################################################
 # extract information from other data.tables to give context to the Conditions
 
-interest_CPM <- exp_design[interest_CPM, on = .(condition)]
+grouped_CPM <- exp_design[grouped_CPM, on = .(condition)]
 
-interest_CPM <- annotated_key[interest_CPM, on = .(name == spacer)]
+grouped_CPM <- annotated_key[grouped_CPM, on = .(name == spacer)]
 
-interest_CPM[!is.na(rep), verbose := paste(media, gDNA_source, growth_condition, rep, sep = "_")]
+grouped_CPM[!is.na(rep), verbose := paste(media, gDNA_source, growth_condition, rep, sep = "_")]
 
-interest_CPM[is.na(rep), verbose := paste(media, gDNA_source, growth_condition, sep = "_")]
+grouped_CPM[is.na(rep), verbose := paste(media, gDNA_source, growth_condition, sep = "_")]
 
 # cross-join between unique conditions and the unique groups of spacers
-# i.e. conditions of interest /x c(controls, knockdowns)
+# i.e. conditions of grouped /x c(controls, knockdowns)
 
-interest_densities <- data.table(merge(interest_CPM[, unique(condition)], interest_CPM[, unique(type)]))
+grouped_densities <- data.table(merge(grouped_CPM[, unique(condition)], grouped_CPM[, unique(type)]))
 
 # the results of a crossjoin are called x and y
 # change the names x and y to something more meaningful
 
-setnames(interest_densities, c("x", "y"), c("condition", "type"))
+setnames(grouped_densities, c("x", "y"), c("condition", "type"))
 
 # !! coincidentally, crossjoins give x and y columns and so do density plots
 # !! the two groups of x and y columns are COMPLETELY unrelated
 # generate NAs in x and y column to create a prototype table
 
-interest_densities[, `:=`(x = NA, y = NA)]
+grouped_densities[, `:=`(x = NA, y = NA)]
 
 ########################################################################################
 # quick, thorough, but complex method to generate density plots from all conditions 
 # run through every "condition" to generate a points for a density plot
 
-for (i in interest_densities[, unique(condition)]) {
+for (i in grouped_densities[, unique(condition)]) {
 	
-	for (j in interest_densities[condition == i, unique(`type`)]) {
+	for (j in grouped_densities[condition == i, unique(`type`)]) {
 		
-		density__ <- density(interest_CPM[condition == i & type == j, log2(CPM)], from = -10, to = 15)
+		density__ <- density(grouped_CPM[condition == i & type == j, log2(CPM)], from = -10, to = 15)
 		
 		density__ <- data.table(condition = i, type = j, x = density__$x, y = density__$y)
 		
-		interest_densities <- merge(interest_densities, density__, all.y = TRUE, all.x = TRUE)
+		grouped_densities <- merge(grouped_densities, density__, all.y = TRUE, all.x = TRUE)
 		
 		rm(density__)
 	}
 }
 
-interest_densities <- interest_densities[complete.cases(interest_densities)]
+grouped_densities <- grouped_densities[complete.cases(grouped_densities)]
 
-interest_densities <- 
-	interest_densities[interest_CPM[, .(
+grouped_densities <- 
+	grouped_densities[grouped_CPM[, .(
 		Condition = unique(Condition)),
 		by = .(condition)], 
 		on = .(condition)]
 
-interest_densities_summary <- 
-	interest_densities[
+grouped_densities_summary <- 
+	grouped_densities[
 		!is.na(Condition), 
 		.(
 			mean_Y = mean(y),
@@ -106,19 +106,19 @@ interest_densities_summary <-
 
 # create a data.table summarizing the conditions across Conditions
 
-interest_densities_summary[is.nan(sd_Y), `:=`(sd_Y = 0, sem_Y = 0)]
+grouped_densities_summary[is.nan(sd_Y), `:=`(sd_Y = 0, sem_Y = 0)]
 
-interest_densities_summary$CI_lower <- interest_densities_summary$mean_Y + qt((1 - 0.85)/2, df = interest_densities_summary$N - 1)*interest_densities_summary$sem_Y
+grouped_densities_summary$CI_lower <- grouped_densities_summary$mean_Y + qt((1 - 0.85)/2, df = grouped_densities_summary$N - 1)*grouped_densities_summary$sem_Y
 
-interest_densities_summary$CI_upper <- interest_densities_summary$mean_Y - qt((1 - 0.85)/2, df = interest_densities_summary$N - 1)*interest_densities_summary$sem_Y
+grouped_densities_summary$CI_upper <- grouped_densities_summary$mean_Y - qt((1 - 0.85)/2, df = grouped_densities_summary$N - 1)*grouped_densities_summary$sem_Y
 
-interest_densities_summary[is.na(CI_lower), CI_lower := mean_Y]
+grouped_densities_summary[is.na(CI_lower), CI_lower := mean_Y]
 
 ########################################################################################
 
-for (i in interest_densities_summary[, unique(Condition)]) {
+for (i in grouped_densities_summary[, unique(Condition)]) {
 	
-	for (j in interest_densities_summary[, unique(type)]) {
+	for (j in grouped_densities_summary[, unique(type)]) {
 
 		if (j == "control") {plot_shade = "blue"}
 		
@@ -126,10 +126,10 @@ for (i in interest_densities_summary[, unique(Condition)]) {
 		
 		plot_object <- 
 			ggplot(
-				interest_densities_summary[Condition == i & type == j], 
+				grouped_densities_summary[Condition == i & type == j], 
 				aes(x = x, y = mean_Y)) +
 			geom_line(
-				data = interest_densities[Condition == i & type == j], 
+				data = grouped_densities[Condition == i & type == j], 
 				aes(x = x, y = y, group = condition), 
 				color = "dark grey") +
 			geom_line(
@@ -141,11 +141,11 @@ for (i in interest_densities_summary[, unique(Condition)]) {
 				fill = plot_shade, 
 				alpha = 0.2) + 
 			xlim(
-				interest_densities_summary[, min(x)], 
-				interest_densities_summary[, max(x)]) +
+				grouped_densities_summary[, min(x)], 
+				grouped_densities_summary[, max(x)]) +
 		ylim(
-			interest_densities_summary[, min(CI_lower, na.rm = T)], 
-			interest_densities_summary[, max(CI_upper, na.rm = T)]) +
+			grouped_densities_summary[, min(CI_lower, na.rm = T)], 
+			grouped_densities_summary[, max(CI_upper, na.rm = T)]) +
 			ggtitle(
 				bquote(Log[2] ~ counts ~ per ~ million. ~ bold(.(i)) ~ italic(.(j)) ~ guides. )) +
 			xlab(
