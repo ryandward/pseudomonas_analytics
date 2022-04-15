1fc0cad48278e870b2e574e2d61603a1b3a3065a
Ryan Ward <ryandward@gmail.com>
2022-04-15 12:51:56 -0500

    begin to conduct forensics on promoters

--- mating_counter.R
+++ mating_counter.R
@@ -6,9 +6,25 @@ additional <- fread(
 		"promoter",
 		"spacer"))
 
-additional[, `:=`(c("strain_promoter", "strain"), tstrsplit(condition, "_"))]
+additional[, `:=`(c("claimed_promoter", "strain"), tstrsplit(condition, "_"))]
 
-additional <- additional[strain_promoter == promoter]
+
+promoter_forensics <-
+	dcast(additional, claimed_promoter + strain  ~ promoter, value.var = "count", fun.aggregate = sum)
+
+promoter_forensics_matrix <- data.matrix(promoter_forensics[, -c(1:2)])
+
+rownames(promoter_forensics_matrix) <- promoter_forensics[, paste(claimed_promoter, strain)]
+
+barplot(
+	t(promoter_forensics_matrix), 
+	beside = T,
+	legend.text = rownames(t(promoter_forensics_matrix)),
+	args.legend = list(x = "top"))
+
+
+
+additional <- additional[claimed_promoter == promoter]
 
 lost_and_found <- 
 	annotated_key[
@@ -38,24 +54,24 @@ additional_grid <-
 
 ################################################################################
 
-plot(density(log2(additional[strain == "P1_mfdpir" & spacer %like% "Ctrl"]$count)), main = "P1 mating", col = "blue", ylim = c(0,0.5))
-lines(density(log2(additional[strain == "P1_mfdpir" & !spacer %like% "Ctrl"]$count)), col = "red")
+plot(density(log10(additional[strain == "P1_mfdpir" & spacer %like% "Ctrl"]$count)), main = "P1 mating", col = "blue", ylim = c(0,0.5))
+lines(density(log10(additional[strain == "P1_mfdpir" & !spacer %like% "Ctrl"]$count)), col = "red")
 
-plot(density(log2(additional[strain == "P1_PA14" & spacer %like% "Ctrl"]$count)), main = "P1 PA14", col = "blue", ylim = c(0,0.5))
-lines(density(log2(additional[strain == "P1_PA14" & !spacer %like% "Ctrl"]$count)), col = "red")
-
-################################################################################
-
-plot(density(log2(additional[strain == "P2_mfdpir" & spacer %like% "Ctrl"]$count)), main = "P2 mating", col = "blue", ylim = c(0,0.5))
-lines(density(log2(additional[strain == "P2_mfdpir" & !spacer %like% "Ctrl"]$count)), col = "red")
-
-plot(density(log2(additional[strain == "P2_PA14" & spacer %like% "Ctrl"]$count)), main = "P2 PA14", col = "blue", ylim = c(0,0.5))
-lines(density(log2(additional[strain == "P2_PA14" & !spacer %like% "Ctrl"]$count)), col = "red")
+plot(density(log10(additional[strain == "P1_PA14" & spacer %like% "Ctrl"]$count)), main = "P1 PA14", col = "blue", ylim = c(0,0.5))
+lines(density(log10(additional[strain == "P1_PA14" & !spacer %like% "Ctrl"]$count)), col = "red")
 
 ################################################################################
 
-plot(density(log2(additional[strain == "P3_mfdpir" & spacer %like% "Ctrl"]$count)), main = "P3 mating", col = "blue", ylim = c(0,0.5))
-lines(density(log2(additional[strain == "P3_mfdpir" & !spacer %like% "Ctrl"]$count)), col = "red")
+plot(density(log10(additional[strain == "P2_mfdpir" & spacer %like% "Ctrl"]$count)), main = "P2 mating", col = "blue", ylim = c(0,0.5))
+lines(density(log10(additional[strain == "P2_mfdpir" & !spacer %like% "Ctrl"]$count)), col = "red")
 
-plot(density(log2(additional[strain == "P3_PA14" & spacer %like% "Ctrl"]$count)), main = "P3 PA14", col = "blue", ylim = c(0,0.5))
-lines(density(log2(additional[strain == "P3_PA14" & !spacer %like% "Ctrl"]$count)), col = "red")
+plot(density(log10(additional[strain == "P2_PA14" & spacer %like% "Ctrl"]$count)), main = "P2 PA14", col = "blue", ylim = c(0,0.5))
+lines(density(log10(additional[strain == "P2_PA14" & !spacer %like% "Ctrl"]$count)), col = "red")
+
+################################################################################
+
+plot(density(log10(additional[strain == "P3_mfdpir" & spacer %like% "Ctrl"]$count)), main = "P3 mating", col = "blue", ylim = c(0,0.5))
+lines(density(log10(additional[strain == "P3_mfdpir" & !spacer %like% "Ctrl"]$count)), col = "red")
+
+plot(density(log10(additional[strain == "P3_PA14" & spacer %like% "Ctrl"]$count)), main = "P3 PA14", col = "blue", ylim = c(0,0.5))
+lines(density(log10(additional[strain == "P3_PA14" & !spacer %like% "Ctrl"]$count)), col = "red")
