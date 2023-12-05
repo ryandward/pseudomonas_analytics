5ee82925f2b91254b7f6c368c7709a8df6e9d155
Ryan Ward <ryandward@gmail.com>
2023-12-05 16:19:34 -0600

    added genes of interest

--- cleanup_volcano.R
+++ cleanup_volcano.R
@@ -69,6 +69,7 @@ genes_of_interest <- c(
 	"pgsA",
 	"orfN",
 	"cysS",
+	"purA",
 	"purB",
 	"purE",
 	"purH",
@@ -84,7 +85,9 @@ genes_of_interest <- c(
 	"lptF",
 	"lptG",
 	"rpoN",
-	"ispD"
+	"ispD",
+	"ispG",
+	"mrfp"
 )
 
 median_melted_results %>% 
@@ -95,12 +98,16 @@ median_melted_results %>%
 		condition == "plated_6_generations_LB - plated_t0_inoculum" ~ "In vitro"
 	)) %>% 
 	ggplot(aes(x = medLFC, y = FDR)) + 
-	geom_point(aes(alpha = FDR < 0.05 & abs(medLFC) > 1), colour = "gray") + 
+	geom_point(
+		aes(
+			alpha = FDR <= 0.05 & abs(medLFC) >= 1,
+			color = gene %in% genes_of_interest)
+		) + 
 	facet_wrap(facets = c("condition")) + 
 	doc_theme + 
 	theme(legend.position = "bottom") +
-	geom_hline(yintercept = 0.05, linetype = "dashed", color = "red") +
-	geom_vline(xintercept = c(-1, 1), linetype = "dashed", color = "red") +
+	geom_hline(yintercept = 0.05, linetype = "dashed", color = "blue") +
+	geom_vline(xintercept = c(-1, 1), linetype = "dashed", color = "blue") +
 	geom_label_repel(
 		data = . %>% 
 			group_by(condition) %>% 
@@ -112,10 +119,12 @@ median_melted_results %>%
 		parse = TRUE,
 		max.overlaps = Inf,
 		aes(
-			label = gene,
-			alpha = FDR < 0.05 & abs(medLFC) > 1)
+			label = gene_name_stylized,
+			color = FDR < 0.05 & abs(medLFC) > 1),
+		alpha =1
 	) +
 	scale_alpha_manual(values = c(0.5, 1), guide = "none") +
+	scale_color_manual(values = c("dark gray", "red"), guide = "none") +
 	scale_y_continuous(trans = scales::reverse_trans() %of% scales::log10_trans())
 
 
